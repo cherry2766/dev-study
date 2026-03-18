a07747e1806c37fd015e55df36b44c22f45a40d8
cherry <rlathrkd2766@gmail.com>
2026-03-18 16:30:25 +0900

    feat: servlet practice

---
+++ backend-study/backend/Servlet01/src/main/java/sec01/FirstServlet.java
@@ -0,0 +1,49 @@
+package sec01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+ 
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출합니다");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * request/response 객체 인스턴스는 톰캣이 생성해서 서블릿 처리 메서드 호출 시 전달하게됨
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet 메서드 호출");
+	
+	}
+
+}
