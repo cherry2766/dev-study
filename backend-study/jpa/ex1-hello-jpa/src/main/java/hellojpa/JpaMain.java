6334ef53081eee615e6af389aa38cb51b2c97104
cherry <rlathrkd2766@gmail.com>
2026-03-28 00:27:32 +0900

    feat: JPA 기본 구조 구현

--- backend-study/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ backend-study/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,23 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+            System.out.println("======================");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
