2f30cdae1507b4bb596c61ff561cd909c874743f
cherry <rlathrkd2766@gmail.com>
2026-03-30 00:19:04 +0900

    feat: AWS S3 이미지 업로드 기능 구현

---
+++ backend-study/practice/upload-practice/src/main/java/com/example/upload/controller/ImageController.java
@@ -0,0 +1,41 @@
+package com.example.upload.controller;
+
+import com.example.upload.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartRequest;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ImageController {
+
+    private ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("/image/upload")
+    public Map<String, Object> imageUpload(MultipartRequest request) throws Exception {
+
+        Map<String, Object> responseData = new HashMap<>();
+
+        try {
+            String s3Url = imageService.imageUpload(request);
+
+            responseData.put("uploaded", true);
+            responseData.put("url", s3Url);
+
+            return responseData;
+
+        } catch (IOException e) {
+            responseData.put("uploaded", false);
+        }
+        return responseData;
+    }
+}
